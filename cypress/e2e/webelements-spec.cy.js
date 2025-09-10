5e94b1d2edf33c8926566656235f43f31a1cfb4b
Antonio Trindade <atrindade@umov.me>
2025-09-09 21:10:21 -0300

    Atualiza configuração do Cypress e ajusta testes de elementos da web

--- cypress/e2e/webelements-spec.cy.js
+++ cypress/e2e/webelements-spec.cy.js
@@ -2,7 +2,7 @@ describe('Trabalhando com elementos da web', () => {
   
     beforeEach(() => {
         //Executa antes de cada it
-        cy.visit('https://antoniotrindade.com.br/treinoautomacao/elementsweb.html')
+        cy.visit('/elementsweb.html')
     })
 
     it('Valida o título da página', () => {
@@ -17,5 +17,23 @@ describe('Trabalhando com elementos da web', () => {
         //2. Realiza a ação
         //3. Verifica o resultado esperado
         cy.get('[name="txtbox1"]').type('Hello world').should('have.value', 'Hello world')
+    })
+
+    it('Valida o componente Label', () => {
+        cy.get('[name="seltestfrm"] > :nth-child(1) > .text-muted')
+            .should('have.text', 'TextField:')
+            .and('be.visible')
+    })
+
+    it('Valida TextField com atributo disabled', () => {
+        cy.get('[name="txtbox1"]')
+            .should('be.enabled')
+            .type('Testando')
+            .should('have.value', 'Testando')
+
+        cy.get('[name="txtbox2"]')
+            .should('be.disabled')
+            .type('Testando', {force: true})
+            .should('have.value', 'Testando') 
     })
 })
