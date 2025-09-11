83f1441430b53b5ccfc46d89d40585bd558e60b5
Antonio Trindade <atrindade@umov.me>
2025-09-11 20:31:09 -0300

    Ajusta o timeout padrão do comando no Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -3,5 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://antoniotrindade.com.br/treinoautomacao",
+    defaultCommandTimeout: 5000,
   },
 });
